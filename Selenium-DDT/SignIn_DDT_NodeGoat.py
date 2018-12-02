869afa1a35c8e78284b195e86c006389bdef8e23
Hsiang-Chih <41407700+Hsiang-Chih@users.noreply.github.com>
2018-12-02 14:37:23 +0800

    Add files via upload

---
+++ Selenium-DDT/SignIn_DDT_NodeGoat.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from ddt import ddt, data, unpack
+import csv
+import unittest, time, re
+
+@ddt
+class NodeGoatSignIn(unittest.TestCase):
+    def get_csv_data(csv_path):
+        rows = []
+        csv_data = open(str(csv_path), "rb")
+        content = csv.reader(csv_data)
+        next(content, None)
+        for row in content:
+            rows.append(row)
+        return rows
+	
+    @classmethod
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    @data(*get_csv_data("sqli.csv"))
+    @unpack
+    def test_fire_sign_in(self, username, password):
+        driver = self.driver
+       
+     
+        driver.get("http://nodegoat.herokuapp.com/login")
+        driver.find_element_by_id("userName").click()
+        driver.find_element_by_id("userName").clear()
+        driver.find_element_by_id("userName").send_keys(username)
+        driver.find_element_by_id("password").click()
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys(password)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+		
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+        
+	
+
+	
+if __name__ == "__main__":
+    unittest.main()
+	
